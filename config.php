3fa6e0f132f846da362ae53fa78f43fc532e6395
ABIEQ <sabiqululum@madrasah.kemenag.go.id>
2023-12-25 13:53:14 +0700

    rdmmadrasah2023

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+$host = "localhost";
+$databaseuser = "root";
+$databasename = "db_rdmma01ypru";
+$dbpassword = "";
+define("DB_SERVER",$host);
+define("DB_USERNAME",$databaseuser);
+define("DB_PASSWORD",$dbpassword);
+define("DB_DATABASE",$databasename);
+?>
